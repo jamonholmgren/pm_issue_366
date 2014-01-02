f445a322a127e854b27697fd74068a99520df86d
Jamon Holmgren <jamon@clearsightstudio.com>
2014-01-02 10:05:19 -0800

    Replicated error.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,7 +2,42 @@ class AppDelegate < PM::Delegate
   status_bar true, animation: :none
 
   def on_load(app, options)
-    open HomeScreen.new(nav_bar: true)
+    open LoginScreen.new(nav_bar: true)
   end
   
+end
+
+# From the ProMotion-formotion README example.
+class LoginScreen < PM::FormotionScreen
+
+  title "Login"
+
+  def on_submit(_form)
+
+  end
+
+  def table_data
+    {
+      sections: [{
+        title: "Credentials",
+        rows: [{
+          title: "Email",
+          key: :email,
+          placeholder: "example@kohactive.com",
+          type: :string,
+          auto_correction: :no,
+          auto_capitalization: :none
+        },
+        {
+          title: "Password",
+          key: :password,
+          placeholder: "",
+          type: :string,
+          auto_correction: :no,
+          auto_capitalization: :none
+        },
+      ]
+      }]
+    }
+  end
 end
